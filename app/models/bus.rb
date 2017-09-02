8ec980f4f2d7a8521fee143fcf1568c3be927669
diego alejandro gomez <diagomezpa@unal.edu.co>
2017-09-02 15:55:11 +0000

    validaciones

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,2 +1,5 @@
 class Bus < ApplicationRecord
+    validates :licenseplate, :capacity, :brand, :startdate, presence: true
+    validates :licenseplate, length: { maximum: 7 }, uniqueness: true
+    validates :capacity, numericality: { less_than:105 }
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,4 @@
 class Client < ApplicationRecord
+    validates :idcard, :name, :benefit, :balance, presence:true
+    validates :idcard, uniqueness:true
 end
